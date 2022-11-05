274d8b14a80e1cade4e7de3dad191b2b0bd7ecc8
Doan Hoang Tinh <tinhdoanhoang@gmail.com>
2022-11-05 23:52:37 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<img src='https://www.w3schools.com/w3images/forestbridge.jpg' alt='background' />
+			<div className='container-content'>
+				<h1>UDACITY </h1>
+				<h1>Doan Hoang Tinh</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
